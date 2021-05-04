08953d2e656b55054c0fa29bf61701e70d93fa65
Raúl Montón Pinillos <raul_m_p@me.com>
2021-05-04 18:34:40 +0200

    Initial Commit

---
+++ PDB Viewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDB Viewer
+//
+//  Created by Raúl Montón Pinillos on 4/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDB Viewer/PDB_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDB_ViewerApp.swift
+//  PDB Viewer
+//
+//  Created by Raúl Montón Pinillos on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PDB_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
