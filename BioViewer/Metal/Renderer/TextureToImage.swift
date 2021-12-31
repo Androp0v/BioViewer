f871f8321ea9347d7bba10b38eae28f87242c2ea
Raúl Montón Pinillos <raul_m_p@me.com>
2021-12-31 12:35:25 +0100

    Fixed a bug with Photo Mode in Release builds due to dangling pointers references being deallocated too soon

--- BioViewer/Metal/Renderer/TextureToImage.swift
+++ BioViewer/Metal/Renderer/TextureToImage.swift
@@ -14,52 +14,77 @@ extension MTLTexture {
 
     func getCGImage(clearBackground: Bool = false, depthTexture: MTLTexture? = nil) -> CGImage? {
 
-        assert(self.pixelFormat == .bgra8Unorm)
+        guard self.pixelFormat == .bgra8Unorm else {
+            NSLog("Unexpected pixelFormat in MTLTexture on getCGImage from MTLTexture.")
+            return nil
+        }
     
-        // Read texture as byte array
+        // MARK: - MTLTexture to array
+        
         let rowBytes = self.width * 4
         let length = rowBytes * self.height
-        var bgraBytes = [UInt8](repeating: 0, count: length)
         let region = MTLRegionMake2D(0, 0, self.width, self.height)
-        self.getBytes(UnsafeMutableRawPointer(mutating: bgraBytes),
-                      bytesPerRow: rowBytes,
-                      from: region,
-                      mipmapLevel: 0)
+        var bgraBytes = [UInt8](repeating: 0, count: length)
         
-        // Clear background
+        // Fill bgraBytes with the drawable texture data.
+        bgraBytes.withUnsafeMutableBytes { bgraBytesPointer in
+            self.getBytes(bgraBytesPointer.baseAddress!,
+                          bytesPerRow: rowBytes,
+                          from: region,
+                          mipmapLevel: 0)
+        }
+        
+        // MARK: - Clear background
+        
         if let depthTexture = depthTexture, clearBackground {
             let depthRowBytes = self.width * 4
-            let depthBytes = [Float](repeating: 0, count: 4 * self.width * self.height)
             let depthRegion = MTLRegionMake2D(0, 0, self.width, self.height)
-            depthTexture.getBytes(UnsafeMutableRawPointer(mutating: depthBytes),
-                                  bytesPerRow: depthRowBytes,
-                                  from: depthRegion,
-                                  mipmapLevel: 0)
-            let bgraPointer = UnsafeMutableRawPointer(mutating: bgraBytes).assumingMemoryBound(to: UInt8.self)
-            let depthPointer = UnsafeMutableRawPointer(mutating: depthBytes).assumingMemoryBound(to: Float32.self)
+            var depthBytes = [Float](repeating: 0, count: 4 * self.width * self.height)
             
-            DispatchQueue.concurrentPerform(iterations: self.width * self.height) { index in
-                let bgraIndex = index * 4 + 3
-                if (depthPointer + index).pointee == 1.0 {
-                    (bgraPointer + bgraIndex).pointee = 0
+            // Fill depthBytes with the texture data.
+            depthBytes.withUnsafeMutableBytes { depthBytesPointer in
+                depthTexture.getBytes(depthBytesPointer.baseAddress!,
+                                      bytesPerRow: depthRowBytes,
+                                      from: depthRegion,
+                                      mipmapLevel: 0)
+                
+                // Modify the bgraBytes according to depth values
+                bgraBytes.withUnsafeMutableBytes { bgraBytesPointer in
+                    let bgraPointer = bgraBytesPointer.baseAddress!.assumingMemoryBound(to: UInt8.self)
+                    let depthPointer = depthBytesPointer.baseAddress!.assumingMemoryBound(to: Float32.self)
+                    DispatchQueue.concurrentPerform(iterations: self.width * self.height) { index in
+                        let bgraIndex = index * 4 + 3
+                        if (depthPointer + index).pointee == 1.0 {
+                            (bgraPointer + bgraIndex).pointee = 0
+                        }
+                    }
                 }
             }
         }
         
+        // MARK: - BGRA to RGBA conversion
         // Use Accelerate framework to convert from BGRA to RGBA
-        var bgraBuffer = vImage_Buffer(data: UnsafeMutableRawPointer(mutating: bgraBytes),
-                                       height: vImagePixelCount(self.height),
-                                       width: vImagePixelCount(self.width),
-                                       rowBytes: rowBytes)
-        let rgbaBytes = [UInt8](repeating: 0, count: length)
-        var rgbaBuffer = vImage_Buffer(data: UnsafeMutableRawPointer(mutating: rgbaBytes),
-                                       height: vImagePixelCount(self.height),
-                                       width: vImagePixelCount(self.width),
-                                       rowBytes: rowBytes)
-        let map: [UInt8] = [2, 1, 0, 3]
-        vImagePermuteChannels_ARGB8888(&bgraBuffer, &rgbaBuffer, map, 0)
-
-        // Create CGImage with RGBA
+        
+        var rgbaBytes = [UInt8](repeating: 0, count: length)
+        bgraBytes.withUnsafeMutableBytes { bgraBytesPointer in
+            var bgraBuffer = vImage_Buffer(data: bgraBytesPointer.baseAddress!,
+                                           height: vImagePixelCount(self.height),
+                                           width: vImagePixelCount(self.width),
+                                           rowBytes: rowBytes)
+            
+            rgbaBytes.withUnsafeMutableBytes { rbgaBytesPointer in
+                var rgbaBuffer = vImage_Buffer(data: rbgaBytesPointer.baseAddress!,
+                                               height: vImagePixelCount(self.height),
+                                               width: vImagePixelCount(self.width),
+                                               rowBytes: rowBytes)
+                let map: [UInt8] = [2, 1, 0, 3]
+                vImagePermuteChannels_ARGB8888(&bgraBuffer, &rgbaBuffer, map, 0)
+            }
+        }
+        
+        // MARK: - CGImage
+        // Create CGImage with RGBA data
+        
         let colorScape = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
         guard let data = CFDataCreate(nil, rgbaBytes, length) else { return nil }
