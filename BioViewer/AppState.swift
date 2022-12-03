9e98358f8a939800feaf5313de3cf41031cfa5b9
Raúl Montón Pinillos <raul_m_p@me.com>
2022-12-03 03:36:07 +0100

    Removed hardcoded value for depth prepass setting

--- BioViewer/AppState.swift
+++ BioViewer/AppState.swift
@@ -47,7 +47,6 @@ class AppState: ObservableObject {
     }
     
     static let hasDepthPrePasses = { () -> Bool in
-        return false
         guard let device = MTLCreateSystemDefaultDevice() else {
             return false
         }
